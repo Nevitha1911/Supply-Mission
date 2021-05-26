1cb38c413e72f3de60572eee1736243846d1e04e
Nevitha1911 <77386752+Nevitha1911@users.noreply.github.com>
2021-05-26 15:15:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload(){
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+    engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	packageSprite=createSprite(width/2, 80, 20,20);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+	translate(packageSprite.x,packageSprite.y)
+
+	Engine.run(engine);
+	world = engine.world
+
+	packageBody = Bodies.circle(width/2 , 200 , 20 , {restitution:0.2, isStatic:true});
+	World.add(world, packageBody);
+
+	box2 = new Box(500,650,20,100,"red");
+	box3 = new Box(295,650,20,100,"red");
+	box1 = new Box(400,690,200,20,"red");
+
+	Engine.run(engine)
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  drawSprites();
+  	box1.display();
+	box2.display();
+	box3.display();
+
+  Engine.update(engine);
+  drawSprites();
+}
+ function keyPressed(){
+	 if(keyCode === LEFT_ARROW){
+		 helicopterSprite.velocityX = -2
+		 translation = {x:-2 , y:0}
+		 Matter.Body.translate(packageBody,translation)
+		 //packageSprite.velocityX = -2
+	 }
+	 if(keyCode === RIGHT_ARROW){
+		 helicopterSprite.velocityX = +2
+		 translation = {x:+2 , y:0}
+		 Matter.Body.translate(packageBody,translation)
+		 //packageSprite.velocityX = +2
+
+	 }
+	 if(keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(packageBody,false);
+		}
+ }
+ 
+
+
+
